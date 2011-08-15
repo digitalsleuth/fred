b7202d5c0a3d9bd88177ba8a66f01a2701b4eed5
GIDA <gida@pingulap>
2011-08-15 21:23:59 +0200

    Small script chnage

--- trunk/report_templates/SOFTWARE_WindowsVersion.js
+++ trunk/report_templates/SOFTWARE_WindowsVersion.js
@@ -5,15 +5,15 @@ println("  <h2>Windows version info</h2>");
 println("  <p>");
 
 var val=GetRegistryKeyValue("\\Microsoft\\Windows NT\\CurrentVersion","ProductName");
-var value_len=val.length;
-print("ProductVersion: ");
-println(RegistryKeyValueToString(val.value,val.type));
 
+print("ProductVersion: ");
+var str_value=RegistryKeyValueToString(val.value,val.type);
+println(str_value);
+
+println("  </p>");
+println("</html>");
 
 //for(var i=0;i<(val.length-1);i+=2) {
 //  print(String.fromCharCode(val.value[i]));
 //}
-
-println("  </p>");
-println("</html>");
 
