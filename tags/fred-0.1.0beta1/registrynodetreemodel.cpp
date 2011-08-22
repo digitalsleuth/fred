e88287ec5ff289d11ad2974d3cf597736ca91df8
Gillen Daniel <gida@gidapc.local>
2011-08-22 08:05:54 +0200

    Tagging v0.1.0beta1

---
+++ tags/fred-0.1.0beta1/registrynodetreemodel.cpp
@@ -0,0 +1,138 @@
+/*******************************************************************************
+* fred Copyright (c) 2011 by Gillen Daniel <gillen.dan@pinguin.lu>             *
+*                                                                              *
+* Forensic Registry EDitor (fred) is a cross-platform M$ registry hive editor  *
+* with special feautures useful during forensic analysis.                      *
+*                                                                              *
+* This program is free software: you can redistribute it and/or modify it      *
+* under the terms of the GNU General Public License as published by the Free   *
+* Software Foundation, either version 3 of the License, or (at your option)    *
+* any later version.                                                           *
+*                                                                              *
+* This program is distributed in the hope that it will be useful, but WITHOUT  *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for     *
+* more details.                                                                *
+*                                                                              *
+* You should have received a copy of the GNU General Public License along with *
+* this program. If not, see <http://www.gnu.org/licenses/>.                    *
+*******************************************************************************/
+#include "registrynodetreemodel.h"
+
+#include <stdlib.h>
+
+RegistryNodeTreeModel::RegistryNodeTreeModel(RegistryHive *p_hive,
+                                             QObject *p_parent)
+  : QAbstractItemModel(p_parent)
+{
+  // Create root node
+  this->p_root_node=new RegistryNode("ROOT");
+  // Build node list
+  this->SetupModelData(p_hive,this->p_root_node);
+}
+
+RegistryNodeTreeModel::~RegistryNodeTreeModel() {
+  delete this->p_root_node;
+}
+
+QVariant RegistryNodeTreeModel::data(const QModelIndex &index, int role) const
+{
+  if(!index.isValid()) return QVariant();
+  if(role!=Qt::DisplayRole) return QVariant();
+
+  RegistryNode *p_node=static_cast<RegistryNode*>(index.internalPointer());
+
+  return p_node->data();
+}
+
+Qt::ItemFlags RegistryNodeTreeModel::flags(const QModelIndex &index) const {
+  if(!index.isValid()) return 0;
+  return Qt::ItemIsEnabled | Qt::ItemIsSelectable;
+}
+
+QVariant RegistryNodeTreeModel::headerData(int section,
+                                           Qt::Orientation orientation,
+                                           int role) const
+{
+  Q_UNUSED(section);
+  if(orientation==Qt::Horizontal && role==Qt::DisplayRole) {
+    return QVariant("Registry key folders");
+  } else {
+    return QVariant();
+  }
+}
+
+QModelIndex RegistryNodeTreeModel::index(int row,
+                                         int column,
+                                         const QModelIndex &parent) const
+{
+  if(!this->hasIndex(row,column,parent)) return QModelIndex();
+
+  RegistryNode *p_parent_node;
+  if(!parent.isValid()) {
+    p_parent_node=this->p_root_node;
+  } else {
+    p_parent_node=static_cast<RegistryNode*>(parent.internalPointer());
+  }
+
+  RegistryNode *p_child_node=p_parent_node->child(row);
+  if(p_child_node) {
+    return this->createIndex(row,column,p_child_node);
+  } else {
+    return QModelIndex();
+  }
+}
+
+QModelIndex RegistryNodeTreeModel::parent(const QModelIndex &index) const {
+  if(!index.isValid()) return QModelIndex();
+
+  RegistryNode *p_child_node=
+    static_cast<RegistryNode*>(index.internalPointer());
+  RegistryNode *p_parent_node=p_child_node->parent();
+
+  if(p_parent_node==this->p_root_node) {
+    return QModelIndex();
+  } else {
+    return this->createIndex(p_parent_node->row(),0,p_parent_node);
+  }
+}
+
+int RegistryNodeTreeModel::rowCount(const QModelIndex &parent) const {
+  if(parent.column()>0) return 0;
+
+  RegistryNode *p_parent_node;
+  if(!parent.isValid()) {
+    p_parent_node=this->p_root_node;
+  } else {
+    p_parent_node=static_cast<RegistryNode*>(parent.internalPointer());
+  }
+
+  return p_parent_node->childCount();
+}
+
+int RegistryNodeTreeModel::columnCount(const QModelIndex &parent) const {
+  Q_UNUSED(parent);
+  return 1;
+}
+
+void RegistryNodeTreeModel::SetupModelData(RegistryHive *p_hive,
+                                           RegistryNode *p_parent,
+                                           int hive_node)
+{
+  QMap<QString,int> hive_children;
+  RegistryNode *p_node;
+
+  // Get all sub nodes of current hive node
+  if(hive_node) hive_children=p_hive->GetNodes(hive_node);
+  else hive_children=p_hive->GetNodes("\\");
+  if(hive_children.isEmpty()) return;
+
+  // Recursivly iterate over all sub nodes
+  QMapIterator<QString, int> i(hive_children);
+  while(i.hasNext()) {
+    i.next();
+    p_node=new RegistryNode(i.key(),p_parent);
+    p_parent->AppendChild(p_node);
+    this->SetupModelData(p_hive,p_node,i.value());
+  }
+}
---
+++ tags/fred-0.1.0beta1/report_templates/SOFTWARE_WindowsVersion.qs
@@ -0,0 +1,82 @@
+function print_table_row(cell01,cell02) {
+  println("      <tr><td>",cell01,"</td><td>",cell02,"</td></tr>");
+}
+
+function DecodeProductKey(arr) {
+  //ProductKey is base24 encoded
+  var keychars=new Array("B","C","D","F","G","H","J","K","M","P","Q","R","T","V","W","X","Y","2","3","4","6","7","8","9");
+  var key=new Array(30);
+  var ret="";
+  var ncur;
+
+  if(arr.length<66) return ret;
+
+  arr=arr.mid(52,15);
+  for(var ilbyte=24;ilbyte>=0;ilbyte--) {
+    ncur=0;
+    for(var ilkeybyte=14;ilkeybyte>=0;ilkeybyte--) {
+      ncur=ncur*256^arr[ilkeybyte];
+      arr[ilkeybyte]=ncur/24;
+      ncur%=24;
+    }
+    ret=keychars[ncur]+ret;
+    if(ilbyte%5==0 && ilbyte!=0) ret="-"+ret;
+  }
+  return ret;
+}
+
+println("<html>");
+println("  <head><title>Windows version info</title></head>");
+println("  <body style=\"font-size:12\">");
+println("  <h2>Windows version info</h2>");
+println("  <p style=\"font-size:12\">");
+println("    <table style=\"margin-left:20px; font-size:12\">");
+
+// Windows version sp and build info
+var val=GetRegistryKeyValue("\\Microsoft\\Windows NT\\CurrentVersion","ProductName");
+print("        <tr><td>Windows version:</td><td>",RegistryKeyValueToString(val.value,val.type));
+var val=GetRegistryKeyValue("\\Microsoft\\Windows NT\\CurrentVersion","CSDVersion");
+if(typeof val !== 'undefined') {
+  print(" ",RegistryKeyValueToString(val.value,val.type));
+}
+var val=GetRegistryKeyValue("\\Microsoft\\Windows NT\\CurrentVersion","CurrentBuildNumber");
+if(typeof val !== 'undefined') {
+  print(" build ",RegistryKeyValueToString(val.value,val.type));
+}
+println("</td></tr>");
+// Build string
+var val=GetRegistryKeyValue("\\Microsoft\\Windows NT\\CurrentVersion","BuildLab");
+print_table_row("Build string:",(typeof val !== 'undefined') ? RegistryKeyValueToString(val.value,val.type) : "n/a");
+// Extended build string
+var val=GetRegistryKeyValue("\\Microsoft\\Windows NT\\CurrentVersion","BuildLabEx");
+print_table_row("Extended build string:",(typeof val !== 'undefined') ? RegistryKeyValueToString(val.value,val.type) : "n/a");
+
+// Install date
+var val=GetRegistryKeyValue("\\Microsoft\\Windows NT\\CurrentVersion","InstallDate");
+print_table_row("Install date:",(typeof val !== 'undefined') ? RegistryKeyValueToVariant(val.value,"unixtime") : "n/a");
+
+// Owner and Organization info
+var val=GetRegistryKeyValue("\\Microsoft\\Windows NT\\CurrentVersion","RegisteredOwner");
+print_table_row("Registered owner:",(typeof val !== 'undefined') ? RegistryKeyValueToString(val.value,val.type) : "n/a");
+var val=GetRegistryKeyValue("\\Microsoft\\Windows NT\\CurrentVersion","RegisteredOrganization");
+print_table_row("Registered organization:",(typeof val !== 'undefined') ? RegistryKeyValueToString(val.value,val.type) : "n/a");
+
+// Windows ID / Key
+var val=GetRegistryKeyValue("\\Microsoft\\Windows NT\\CurrentVersion","ProductId");
+print_table_row("Product ID:",(typeof val !== 'undefined') ? RegistryKeyValueToString(val.value,val.type) : "n/a");
+var val=GetRegistryKeyValue("\\Microsoft\\Windows NT\\CurrentVersion","DigitalProductId");
+if(typeof val !== 'undefined') {
+  var key=DecodeProductKey(val.value);
+  if(key!="BBBBB-BBBBB-BBBBB-BBBBB-BBBBB") print_table_row("Product Key:",key);
+  else print_table_row("Product Key:","n/a (Probably a volume license key was used)");
+} else print_table_row("Product Key:","n/a");
+
+// Install directory / Source directory
+var val=GetRegistryKeyValue("\\Microsoft\\Windows NT\\CurrentVersion","PathName");
+print_table_row("Install path:",(typeof val !== 'undefined') ? RegistryKeyValueToString(val.value,val.type) : "n/a");
+var val=GetRegistryKeyValue("\\Microsoft\\Windows NT\\CurrentVersion","SourcePath");
+print_table_row("Source path:",(typeof val !== 'undefined') ? RegistryKeyValueToString(val.value,val.type) : "n/a");
+
+println("    </table>");
+println("  </p>");
+println("</html>");
