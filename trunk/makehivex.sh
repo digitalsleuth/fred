828df200c1d67c8c67f3d6e65249f3d50386b98c
Gillen Daniel <gidapc.local>
2013-06-20 14:44:48 +0200

    BugFix: makehivex.sh: Fixed typo.

--- trunk/makehivex.sh
+++ trunk/makehivex.sh
@@ -3,11 +3,10 @@
 # When compiling under mingw, make sure PKG_CONFIG_PATH is set
 uname | grep MINGW &>/dev/null
 if [ $? -eq 0 ]; then
-  export PKG_CONFUG_PATH="/local/lib/pkgconfig"
+  export PKG_CONFIG_PATH="/local/lib/pkgconfig"
 fi
 
 cd hivex
-make clean
 autoreconf
 ./configure --disable-ocaml --disable-perl --disable-python --disable-ruby && make
 cd ..
